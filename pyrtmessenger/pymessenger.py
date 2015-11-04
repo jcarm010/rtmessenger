4cb7e76974fe6d06716ab59917944e34af6ed1b3
Javier Carmona <jcarm010@fiu.edu>
2015-11-03 22:09:32 -0500

    added subscribe request

--- pyrtmessenger/pymessenger.py
+++ pyrtmessenger/pymessenger.py
@@ -1,15 +1,33 @@
 import sys
 from socketIO_client import SocketIO, LoggingNamespace
+import time
 
 __author__ = 'javier'
-def main(argv):
-    def on_bbb_response(*args):
+class messenger:
+    socketIO = None
+
+    def __init__(self, host, port):
+        self.socketIO = SocketIO(host, port, LoggingNamespace)
+
+    def subscribe(self, channel_name):
+        self.socketIO.emit(
+            'request',
+            {
+                'action': 'subscribe',
+                'channel': channel_name
+            },
+            self.on_request_response
+        )
+        self.socketIO.wait_for_callbacks()
+
+    def on_request_response(self, *args):
         print('on_bbb_response', args)
 
-    with SocketIO('localhost', 3000, LoggingNamespace) as socketIO:
-        socketIO.emit('chat message', {'xxx': 'yyy'}, on_bbb_response)
-        socketIO.wait_for_callbacks(seconds=1)
-
+def main(argv):
+    m = messenger("localhost", 3000)
+    m.subscribe("TestChannel")
+    while True:
+        time.sleep(10)
 
 if __name__ == '__main__':
     main(sys.argv)
--- rtmessenger_server/index.js
+++ rtmessenger_server/index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var channel_connections = {};
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,10 +13,37 @@ io.on('connection', function(socket){
   socket.on('disconnect', function(){
     console.log('user disconnected');
   });
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-    io.emit('chat message', msg);
+  socket.on('request', function(request){
+    console.log('message: ');
+    console.log(request);
+    try{
+      //io.emit('chat message', msg);
+      if(request.action === "subscribe"){
+        console.log("Requested subscription");
+        handleSubscribeRequest(request, socket);
+      }else{
+        throw "Invalid action type";
+      }
+      console.log("current channel connections:");
+      for(var cname in channel_connections) {
+        console.log(cname+": "+channel_connections[cname].length);
+      }
+    }catch (error){
+      console.error(error);
+      socket.disconnect()
+    }
   });
+  function handleSubscribeRequest(request, socket){
+    var channelName = request.channel;
+    if(channelName === null || channelName === undefined){
+      throw "Invalid channel name: "+channelName;
+    }
+    var sockets = channel_connections[channelName];
+    if(sockets === undefined){
+      sockets = channel_connections[channelName] = [];
+    }
+    sockets.push(socket);
+  }
 });
 
 http.listen(3000, function(){
