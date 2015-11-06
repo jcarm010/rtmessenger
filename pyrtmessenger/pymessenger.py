005af61afb8dee688ec1bba9dad90b68273d9de7
Javier Carmona <jcarm010@fiu.edu>
2015-11-05 22:25:46 -0500

    finished publish/subscribe/presence wiring

--- pyrtmessenger/pymessenger.py
+++ pyrtmessenger/pymessenger.py
@@ -1,31 +1,87 @@
 import sys
-from socketIO_client import SocketIO, LoggingNamespace
+import threading
+from socketIO_client import SocketIO, BaseNamespace
 import time
 
 __author__ = 'javier'
-class messenger:
-    socketIO = None
 
-    def __init__(self, host, port):
-        self.socketIO = SocketIO(host, port, LoggingNamespace)
+class RTMessenger:
+    def __init__(self, host, port, callback=None):
+        self.on_connect_callback = callback
+        self.subscribed_callbacks = {}
+        self.message_callbacks = {}
+        self.presence_callbacks={}
+        self.socketIO = SocketIO(host, port)
+        self.socketIO.on('connect', self.on_connect)
+        self.socketIO.on('subscribed', self.on_subscribed)
+        waiter = threading.Thread(target=self.waiter)
+        waiter.daemon = True
+        waiter.start()
 
-    def subscribe(self, channel_name):
+    def on_message(self, message):
+        channel = message['channel']
+        if message['presence']:
+            callback = self.presence_callbacks[channel]
+            if callback is not None:
+                callback(channel, message['message'])
+        else:
+            callback = self.message_callbacks[channel]
+            if callback is not None:
+                callback(channel, message['message'])
+
+    def on_connect(self):
+        if self.on_connect_callback is not None:
+            self.on_connect_callback()
+
+    def on_subscribed(self, message):
+        channel = message['channel']
+        callback = self.subscribed_callbacks[channel]
+        if callback is not None:
+            callback(channel)
+
+    def waiter(self):
+        self.socketIO.wait()
+
+    def subscribe(self, channel, message_callback,presence_callback=None, callback=None):
         self.socketIO.emit(
             'request',
             {
                 'action': 'subscribe',
-                'channel': channel_name
-            },
-            self.on_request_response
+                'channel': channel
+            }
         )
-        self.socketIO.wait_for_callbacks()
+        self.subscribed_callbacks[channel] = callback
+        self.message_callbacks[channel] = message_callback
+        self.presence_callbacks[channel] = presence_callback
+        self.socketIO.on(channel, self.on_message)
 
-    def on_request_response(self, *args):
-        print('on_bbb_response', args)
+    def publish(self, channel, message):
+        self.socketIO.emit(
+            'request',
+            {
+                'action': 'message',
+                'channel': channel,
+                'message': message
+            }
+        )
+
+
+def on_connect():
+    print("Connected!")
+
+def on_subscribe(channel):
+    print("Subscribed to channel: "+channel)
+
+def on_message(channel, message):
+    print("Message received on channel "+channel+": "+str(message))
+
+def on_presence(channel, message):
+    print("Presence received on channel " + channel + ": " + str(message))
 
 def main(argv):
-    m = messenger("localhost", 3000)
-    m.subscribe("TestChannel")
+    m = RTMessenger("localhost", 3000, on_connect)
+    m.subscribe("TestChannel", on_message, presence_callback=on_presence , callback=on_subscribe)
+    m.publish("TestChannel", {"say": "hello world"})
     while True:
         time.sleep(10)
 
--- rtmessenger_server/index.js
+++ rtmessenger_server/index.js
@@ -3,15 +3,22 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var channel_connections = {};
-
+var socket_connections = {};
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket_connections[socket] = [];
   socket.on('disconnect', function(){
     console.log('user disconnected');
+    var channels = socket_connections[socket];
+    for(var i = 0 ; i < channels.length; i++){
+      var channel = channels[i];
+      io.emit(channel, {channel: channel, presence:true, message: {name:'test', action:"left"}});
+    }
+    delete socket_connections[socket];
   });
   socket.on('request', function(request){
     console.log('message: ');
@@ -21,6 +28,8 @@ io.on('connection', function(socket){
       if(request.action === "subscribe"){
         console.log("Requested subscription");
         handleSubscribeRequest(request, socket);
+      }else if(request.action === "message"){
+        handleMessage(request, socket);
       }else{
         throw "Invalid action type";
       }
@@ -33,9 +42,14 @@ io.on('connection', function(socket){
       socket.disconnect()
     }
   });
+  function handleMessage(request, socket){
+    var channelName = request.channel;
+    io.emit(channelName, {channel: channelName,presence:false, message: request.message});
+  }
   function handleSubscribeRequest(request, socket){
     var channelName = request.channel;
     if(channelName === null || channelName === undefined){
+      socket.emit("subscribed", {success:false, channel: channelName, message:"Invalid channel name: "+channelName});
       throw "Invalid channel name: "+channelName;
     }
     var sockets = channel_connections[channelName];
@@ -43,6 +57,9 @@ io.on('connection', function(socket){
       sockets = channel_connections[channelName] = [];
     }
     sockets.push(socket);
+    socket_connections[socket].push(channelName);
+    socket.emit("subscribed", {success:true, channel:channelName});
+    io.emit(channelName, {channel: channelName, presence:true, message: {name:'test', action:"joined"}});
   }
 });
 
